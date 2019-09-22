ab282cf105a051e530da862b9cebc250c92b1753
ArielBarros <ariel@resolvvi.com>
2019-09-22 16:53:54 -0300

    Add eventListener code example

---
+++ src/listener.js
@@ -0,0 +1,17 @@
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:7545'));
+const Marketplace = require('./abis/Marketplace.json');
+
+eventListener = async () => {
+  const networkId = await web3.eth.net.getId();
+  const networkData = Marketplace.networks[networkId];
+  const marketplace = await web3.eth.Contract(Marketplace.abi, networkData.address);
+  
+  console.log('Listening events...');
+  marketplace.events.Buy().on('data', event => {
+    console.log('Evento data')
+    console.log(event);
+  });
+}
+
+eventListener();
