5c5c17095d79a26944ebc757741536c317a04938
ArielBarros <ariel@resolvvi.com>
2019-11-03 17:33:47 -0300

    Handle utility company account in ropsten deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Marketplace = artifacts.require("Marketplace");
 
 module.exports = function(deployer, network, accounts) {
@@ -6,7 +7,13 @@ module.exports = function(deployer, network, accounts) {
   const _decimals = 18;
   const _initialSupply = 1000000;
   const _tokenPrice = 1000000000000000; // Token price is 0.001 Ether
-  const _utilityCompany = accounts[3];
+
+  let _utilityCompany;
+  if (network == 'ropsten') {
+    _utilityCompany = process.env.UTILITY_COMPANY_ACCOUNT;
+  } else {
+    _utilityCompany = accounts[3];
+  }
 
   deployer.deploy(Marketplace, _name, _symbol, _decimals, _initialSupply, _tokenPrice, _utilityCompany);
 };
